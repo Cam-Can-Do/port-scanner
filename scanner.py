8b0565f4ff8673c023c5933068913d7a978e735f
Cameron Candau <camcandau@gmail.com>
2023-05-29 15:22:06 -0700

    Works w cmd line arguments to scan 1 or more ports

--- scanner.py
+++ scanner.py
@@ -1,13 +1,12 @@
-import socket
-import subprocess
 import sys
-from datetime import datetime
+import socket
 
+# returns true if the specified port on the host network is open
+# returns false if it is closed
 def portOpen(hostIP, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(5)
     try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        #s.settimeout(5)
-
         if s.connect_ex((hostIP, port)):
             return False
         else:
@@ -15,85 +14,46 @@ def portOpen(hostIP, port):
     except socket.gaierror:
         print("Hostname could not be resolved. Exiting")
         sys.exit()
-
     except socket.error:
-        print ("Couldn't connect to server")
+        print("Couldn't connect to server")
         sys.exit()
+    finally: 
+        s.close()
 
-    
-
-def scanAll(hostIP):
-
-    # starting datetime
-    t1 = datetime.now()
-
-    for port in range(1, 5000):
+# scans ports over the specified range and prints results
+def scanPorts(hostIP, start_port, end_port):
+    for port in range(start_port, end_port + 1):
         result = portOpen(hostIP, port)
         printResult(port, result)
-        #s.close()
 
+# prints whether a port is open or closed (depending on commented lines)
+def printResult(port, result):    
+    # Only print open ports
+    if result:
+        print(f"Port {port} is open.")
 
-    #check end datetime
-    t2 = datetime.now()
-
-    total = t2 - t1
-
-    print("Scanning Completed in: ", total)
-
-def printResult(port, result):
-    print(f"Port {port} is", ("open." if result else "closed."))
-
-
+    # Print closed and open ports
+    # print(f"Port {port} is", ("open." if result else "closed."))
 
 def main():
-    '''
-    # clear terminal screen
-    subprocess.call('clear', shell=True)
-    '''
+    # incorrect usage check and message
+    if len(sys.argv) < 3:
+        print("Usage: python3 portscanner.py <host> <port(s)>")
+        sys.exit(1)
 
-    # input
-    host = input("Remote host to scan: ")
+    # get hostname, which is the second argument and its IP
+    host = sys.argv[1]
     hostIP = socket.gethostbyname(host)
 
-    #port = int(input("Port to scan: "))
-
-
-    #subprocess.call('clear', shell=True)
-
-    # print banner with information
-    #print("_" * 60) 
-    #print(f"Scanning remote host {hostIP} on port {port}")
-    #print("-" * 60) 
-
-    scanAll(hostIP)
-'''
-    # starting datetime
-    t1 = datetime.now()
-
-    try:
-        for port in range(1, 5000):
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            result = s.connect_ex((remoteServerIP, port))
-            if result == 0:
-                print (f"Port {port}:    Open")
-            sock.close()
-
-
-    except socket.gaierror:
-        print("Hostname could not be resolved. Exiting")
-        sys.exit()
-
-    except socket.error:
-        print ("Couldn't connect to server")
-        sys.exit()
-
-    #check end datetime
-    t2 = datetime.now()
-
-    total = t2 - t1
-
-    print("Scanning Completed in: ", total)
-'''
+    # remaining arguments are port range(s) to scan
+    ports = sys.argv[2:]
+    for port in ports:
+        # check if a range, indicated by containing a "-"
+        if "-" in port:
+            start_port, end_port = map(int, port.split("-"))
+            scanPorts(hostIP, start_port, end_port)
+        else:
+            scanPorts(hostIP, int(port), int(port))
 
 if __name__ == "__main__":
     main()
